1151593f4e20fdde59c119e186d7a0088b46a2da
Bikram Pandey <142283760+bikrampandey@users.noreply.github.com>
2024-06-11 16:11:21 +1200

    member page changes

--- WPF Library assessment/Window stuff/MemberInfoWN.xaml.cs
+++ WPF Library assessment/Window stuff/MemberInfoWN.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,15 +14,83 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace WPF_Library_assessment.Window_stuff
-{
-    /// <summary>
+{   /// <summary>
     /// Interaction logic for MemberInfoWN.xaml
     /// </summary>
+    /// 
     public partial class MemberInfoWN : Window
     {
+        
+        private ObservableCollection<Member> members = new ObservableCollection<Member>();
+
         public MemberInfoWN()
         {
             InitializeComponent();
+
+            // Bind the members collection to the ListBox
+            MembersListBox.ItemsSource = members;
         }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            Member newMember = new Member()
+            {
+                FirstName = FirstNameTextBox.Text,
+                LastName = LastNameTextBox.Text,
+                Email = EmailTextBox.Text,
+                Age = int.Parse(AgeTextBox.Text),
+                PhoneNumber = PhoneNumberTextBox.Text,
+                Address = AddressTextBox.Text,
+                Username = UsernameTextBox.Text,
+                Password = PasswordBox.Password
+            };
+
+            // Add the new member 
+            members.Add(newMember);
+
+            // Clear the text boxes
+            ClearTextBoxes();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (MembersListBox.SelectedItem != null)
+            {
+                
+                members.Remove((Member)MembersListBox.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Please select a member to delete.");
+            }
+        }
+
+        private void ClearTextBoxes()
+        {
+            
+            FirstNameTextBox.Text = "";
+            LastNameTextBox.Text = "";
+            EmailTextBox.Text = "";
+            AgeTextBox.Text = "";
+            PhoneNumberTextBox.Text = "";
+            AddressTextBox.Text = "";
+            UsernameTextBox.Text = "";
+            PasswordBox.Password = "";
+        }
+    }
+
+    // Define a class to represent a member
+    public class Member
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
