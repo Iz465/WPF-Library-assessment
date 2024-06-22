f0126f9f8cee66b5a6b424e5d84d1a5e483cb03a
Isak <Isakmolvviiing@gmail.com>
2024-06-23 00:13:11 +1200

    editing book page

--- WPF Library assessment/Library Pages/BookPG.xaml.cs
+++ WPF Library assessment/Library Pages/BookPG.xaml.cs
@@ -5,25 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WPF_Library_assessment.Mongo_Info;
 using WPF_Library_assessment.Window_stuff;
 
 namespace WPF_Library_assessment.Library_Pages
 {
-
     public partial class BookPG : Page
     {
         public BookPG()
         {
             InitializeComponent();
-
 
             MongoData mongoData = new MongoData();
             List<Books> horrorBooks = mongoData.Connect<Books>("Horror");
@@ -31,25 +24,19 @@ namespace WPF_Library_assessment.Library_Pages
             List<Books> DramaBooks = mongoData.Connect<Books>("Drama");
             List<Books> MysteryBooks = mongoData.Connect<Books>("Mystery");
 
-
-         //   int row = addInfo(horrorBooks, "Horror", "Fantasy", 4); row++;
-         //   row = addInfo(fantasyBooks, "Fantasy", "Drama", row); row++;
-          //  row = addInfo(DramaBooks, "Drama", "Mystery", row); row++;
-          //  row = addInfo(MysteryBooks, "Mystery", "", row);
+            int row = addInfo(horrorBooks, "Horror", "Fantasy", 2); row++;
+            row = addInfo(fantasyBooks, "Fantasy", "Drama", row); row++;
+            row = addInfo(DramaBooks, "Drama", "Mystery", row); row++;
+            row = addInfo(MysteryBooks, "Mystery", "", row);
         }
-   /*     public int addInfo(List<Books> bookType, string collectionName, string genre, int rowNum)
+
+        public int addInfo(List<Books> bookType, string collectionName, string genre, int rowNum)
         {
-
-
             foreach (var book in bookType)
             {
                 createRow(InfoGrid);
                 TextBlock nameText = CreateTextBlock(book.Name);
                 TextBlock authorText = CreateTextBlock(book.Author);
-                TextBlock pagesText = CreateTextBlock(book.Pages.ToString());
-                TextBlock availableText = CreateTextBlock(book.Available);
-                TextBlock overdueText = CreateTextBlock(book.Overdue);
-                TextBlock timeText = CreateTextBlock(book.Time.ToString());
                 Button updateButton = CreateButton("Update");
                 Button deleteButton = CreateButton("Delete");
 
@@ -60,23 +47,20 @@ namespace WPF_Library_assessment.Library_Pages
                 deleteButton.Click += DeleteButton_Click;
 
                 AddElementToGrid(nameText, rowNum, 0, InfoGrid);
-                AddElementToGrid(authorText, rowNum, 1, InfoGrid);
-                AddElementToGrid(pagesText, rowNum, 2, InfoGrid);
-                AddElementToGrid(availableText, rowNum, 3, InfoGrid);
-                AddElementToGrid(overdueText, rowNum, 4, InfoGrid);
-                AddElementToGrid(timeText, rowNum, 5, InfoGrid);
-                AddElementToGrid(updateButton, rowNum, 6, InfoGrid);
-                AddElementToGrid(deleteButton, rowNum, 7, InfoGrid);
+                AddElementToGrid(authorText, rowNum, 2, InfoGrid);
+                AddElementToGrid(updateButton, rowNum, 4, InfoGrid);
+                AddElementToGrid(deleteButton, rowNum, 6, InfoGrid);
 
                 rowNum++;
             }
 
-            TextBlock genreText = new TextBlock();
-            genreText.Text = genre;
-            genreText.FontSize = 30;
-            genreText.Margin = new Thickness(0, 0, 0, 10);
-            genreText.HorizontalAlignment = HorizontalAlignment.Center;
-            genreText.VerticalAlignment = VerticalAlignment.Center;
+            TextBlock genreText = new TextBlock
+            {
+                Text = genre,
+                FontSize = 30,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
 
             Grid.SetRow(genreText, rowNum);
             Grid.SetColumn(genreText, 1);
@@ -88,13 +72,10 @@ namespace WPF_Library_assessment.Library_Pages
 
         public void createRow(Grid gridName)
         {
-
             RowDefinition row = new RowDefinition();
-            row.Height = new GridLength(40);
+            row.Height = new GridLength(60);
             gridName.RowDefinitions.Add(row);
-
         }
-
 
         public TextBlock CreateTextBlock(string text)
         {
@@ -102,9 +83,8 @@ namespace WPF_Library_assessment.Library_Pages
             {
                 Text = text,
                 FontSize = 16,
-                Margin = new Thickness(10, 0, 0, 0),
                 HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Bottom
+                VerticalAlignment = VerticalAlignment.Center,
             };
         }
 
@@ -114,30 +94,23 @@ namespace WPF_Library_assessment.Library_Pages
             {
                 Content = new TextBlock { Text = content },
                 FontSize = 16,
-                Width = 90,
-                Height = 90,
+                Padding = new Thickness(10),
                 HorizontalAlignment = HorizontalAlignment.Center,
                 VerticalAlignment = VerticalAlignment.Center,
-                Style = (Style)FindResource("mainbutton"),
-
             };
         }
-
 
         public void AddElementToGrid(UIElement element, int row, int column, Grid gridName)
         {
             Grid.SetRow(element, row);
             Grid.SetColumn(element, column);
+            Grid.SetColumnSpan(element, 2);
             gridName.Children.Add(element);
         }
 
-
-
-
         public void UpdateButton_Click(object sender, RoutedEventArgs e)
         {
-
-            Button? updatebtn = sender as Button;
+            Button updatebtn = sender as Button;
             Tuple<string, string, string, string, string, string> tagData = updatebtn.Tag as Tuple<string, string, string, string, string, string>;
 
             string collectionName = tagData.Item1;
@@ -146,29 +119,18 @@ namespace WPF_Library_assessment.Library_Pages
             string author = tagData.Item4;
             string pages = tagData.Item5;
             string available = tagData.Item6;
-            //    string overdue = tagData.Item7;
-            //   string time = tagData.Item8;
+
             UpdateWN updateWN = new UpdateWN();
             updateWN.changeBook(collectionName, bookId, name, author, pages, available);
 
             updateWN.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             updateWN.WindowStyle = WindowStyle.None;
             updateWN.Show();
-
-
         }
-
-
-
 
         public void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
-
-
-
-            Button? deleteBtn = sender as Button;
-
-
+            Button deleteBtn = sender as Button;
 
             Tuple<string, string> tagData = deleteBtn.Tag as Tuple<string, string>;
             string collectionName = tagData.Item1;
@@ -177,10 +139,8 @@ namespace WPF_Library_assessment.Library_Pages
             MongoData mongoData = new MongoData();
             mongoData.DeleteCollection<Books>(collectionName, bookId);
 
-
             int row = Grid.GetRow(deleteBtn);
             InfoGrid.Children.Remove(deleteBtn);
-
 
             foreach (UIElement element in InfoGrid.Children.Cast<UIElement>().ToList())
             {
@@ -194,10 +154,9 @@ namespace WPF_Library_assessment.Library_Pages
         public void Button_Click(object sender, RoutedEventArgs e)
         {
             NewBook newBook = new NewBook();
-            newBook.WindowStartupLocation = (WindowStartupLocation.CenterScreen);
+            newBook.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             newBook.WindowStyle = WindowStyle.None;
             newBook.Show();
-
-        } */
+        }
     }
 }
