b7dafa62797451b8816b162494a1d7ede0ef03a6
Isak <Isakmolvviiing@gmail.com>
2024-06-25 01:05:20 +1200

    update

--- WPF Library assessment/Library Pages/MemberPG.xaml.cs
+++ WPF Library assessment/Library Pages/MemberPG.xaml.cs
@@ -28,61 +28,163 @@ namespace WPF_Library_assessment.Library_Pages
             InitializeComponent();
 
             
-       //         MongoData mongoData = new MongoData();
-        //        List<Members> members = mongoData.Connect<Members>("Members");
-
-         //       AddInfo(members, "Members", 3);
+                MongoData mongoData = new MongoData();
+                BookPG bookPG = new BookPG();
+                List<Members> members = mongoData.Connect<Members>("Members");
+                addInfo(members, "Members", 0);
             
         
 
 
 
-        }
-         /*
+        }       
       
-       public void AddInfo(List<Members> memberList, string collectionName, int row)
+       public void addInfo(List<Members> memberList, string collectionName, int row)
         {
             BookPG bookPG = new BookPG();
 
             foreach ( var person in memberList)
             {
-                MessageBox.Show(row.ToString());
-                bookPG.createRow(MemberGrid);
-
-                
-                TextBlock FirstName = bookPG.CreateTextBlock(person.FirstName);
-                TextBlock LastName = bookPG.CreateTextBlock(person.LastName);
-                TextBlock Age = bookPG.CreateTextBlock(person.Age.ToString());
-                TextBlock Phone = bookPG.CreateTextBlock(person.PhoneNumber.ToString());
-                TextBlock Address = bookPG.CreateTextBlock(person.Address);
+             
+                bookPG.createRow(MembersGrid,50);
+               
                 TextBlock Username = bookPG.CreateTextBlock(person.Username);
                 TextBlock Password = bookPG.CreateTextBlock(person.Password);
                 Button UpdateBtn = bookPG.CreateButton("Update");
                 Button DeleteBtn = bookPG.CreateButton("Delete");
+           //     UpdateBtn.Tag = new Tuple<string, string, string, string, string, string>
+                 //      (collectionName, person.Id.ToString(), person.FirstName, person.LastName, book.Pages.ToString(), book.Available);
+            //    UpdateBtn.Click += UpdateButton_Click;
+            //    DeleteBtn.Tag = new Tuple<string, string>(collectionName, book.Id.ToString());
+            //    DeleteBtn.Click += DeleteButton_Click;
+                bookPG.AddElementToGrid(Username, row, 5, MembersGrid);
+                bookPG.AddElementToGrid(Password, row, 6, MembersGrid);
+                bookPG.AddElementToGrid(UpdateBtn, row, 7, MembersGrid);
+                bookPG.AddElementToGrid(DeleteBtn, row, 8, MembersGrid);
 
-                
+                row++;
+                bookPG.createRow(MembersGrid, 20);
 
-
-                
-                bookPG.AddElementToGrid(FirstName,row, 0, MemberGrid);
-                bookPG.AddElementToGrid(LastName, row, 1,MemberGrid);
-                bookPG.AddElementToGrid(Age, row, 2, MemberGrid);
-                bookPG.AddElementToGrid(Phone, row, 3, MemberGrid);
-                bookPG.AddElementToGrid(Address, row, 4, MemberGrid);
-                bookPG.AddElementToGrid(Username, row, 5, MemberGrid);
-                bookPG.AddElementToGrid(Password, row, 6, MemberGrid);
-                bookPG.AddElementToGrid(UpdateBtn, row, 7, MemberGrid);
-                bookPG.AddElementToGrid(DeleteBtn, row, 8, MemberGrid);
-
+                System.Windows.Shapes.Rectangle rectangle = new System.Windows.Shapes.Rectangle(); // converting it to proper one 
+                rectangle.Height = 5;
+                rectangle.Fill = Brushes.White;
+                Grid.SetColumnSpan(rectangle, 6);
+                bookPG.AddElementToGrid(rectangle, row, 0, MembersGrid);
                 row++;
 
             }
 
 
           
-        } */
+        } 
 
 
 
         }
 }
+
+/*   <Page.Resources>
+        <Style TargetType="TextBlock" x:Key="SmallHeaderTextStyle">
+            <Setter Property="FontSize" Value="28"/>
+            <Setter Property="FontWeight" Value="Bold"/>
+            <Setter Property="HorizontalAlignment" Value="Center"/>
+            <Setter Property="VerticalAlignment" Value="Top"/>
+            <Setter Property="Margin" Value="10"/>
+            <Setter Property="Foreground" Value="LawnGreen"/>
+        </Style>
+        <Style TargetType="TextBlock" x:Key="BookInfoLabelStyle">
+            <Setter Property="FontSize" Value="16"/>
+            <Setter Property="Margin" Value="5"/>
+            <Setter Property="VerticalAlignment" Value="Center"/>
+            <Setter Property="Foreground" Value="LawnGreen"/>
+        </Style>
+        <Style TargetType="TextBox" x:Key="BookInfoTextBoxStyle">
+            <Setter Property="FontSize" Value="16"/>
+            <Setter Property="Margin" Value="9"/>
+            <Setter Property="VerticalAlignment" Value="Center"/>
+            <Setter Property="Width" Value="300"/>
+            <Setter Property="HorizontalAlignment" Value="Left"/>
+            <Setter Property="Padding" Value="8"/>
+            <Setter Property="Background" Value="White"/>
+            <Setter Property="BorderBrush" Value="#D3D3D3"/>
+            <Setter Property="BorderThickness" Value="1"/>
+            <Setter Property="Foreground" Value="LawnGreen"/>
+        </Style>
+        <Style TargetType="Button" x:Key="ActionButtonStyle">
+            <Setter Property="FontSize" Value="14"/>
+            <Setter Property="FontWeight" Value="Bold"/>
+            <Setter Property="Width" Value="100"/>
+            <Setter Property="Height" Value="30"/>
+            <Setter Property="Margin" Value="8"/>
+            <Setter Property="HorizontalAlignment" Value="Center"/>
+            <Setter Property="Background" Value="DarkOliveGreen"/>
+            <Setter Property="Foreground" Value="LawnGreen"/>
+        </Style>
+            
+        <ImageBrush x:Key="PageBackgroundBrush" ImageSource="/images/mback.jpg" Stretch="UniformToFill"/>
+        
+    </Page.Resources>
+
+    <Grid x:Name="MemberGrid" Margin="10" Opacity="0.9" VerticalAlignment="Center">
+
+        <Grid.Background>
+            <ImageBrush ImageSource="..\images\mback.jpg"/>
+        </Grid.Background>
+
+
+
+
+
+        <Grid.RowDefinitions>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+        </Grid.RowDefinitions>
+        <Grid.ColumnDefinitions>
+            <ColumnDefinition Width="Auto"/>
+            <ColumnDefinition Width="Auto"/>
+            <ColumnDefinition Width="Auto"/>
+        </Grid.ColumnDefinitions>
+
+        <!-- Small Header -->
+        <TextBlock Text="Member Info" Style="{StaticResource SmallHeaderTextStyle}" Grid.Row="0" Grid.Column="3" Grid.ColumnSpan="3" HorizontalAlignment="Center"/>
+
+        <!-- First Name -->
+        <TextBlock Text="First Name:" Style="{StaticResource BookInfoLabelStyle}" Grid.Row="1" Grid.Column="0"/>
+        <TextBox x:Name="FirstNameTextBox" Style="{StaticResource BookInfoTextBoxStyle}" Grid.Row="1" Grid.Column="1" Grid.ColumnSpan="2"/>
+
+        <!-- Last Name -->
+        <TextBlock Text="Last Name:" Style="{StaticResource BookInfoLabelStyle}" Grid.Row="2" Grid.Column="0"/>
+        <TextBox x:Name="LastNameTextBox" Style="{StaticResource BookInfoTextBoxStyle}" Grid.Row="2" Grid.Column="1" Grid.ColumnSpan="2"/>
+
+        <!-- Age -->
+        <TextBlock Text="Age:" Style="{StaticResource BookInfoLabelStyle}" Grid.Row="3" Grid.Column="0"/>
+        <TextBox x:Name="AgeTextBox" Style="{StaticResource BookInfoTextBoxStyle}" Grid.Row="3" Grid.Column="1" Grid.ColumnSpan="2"/>
+
+        <!-- Phone Number -->
+        <TextBlock Text="Phone Number:" Style="{StaticResource BookInfoLabelStyle}" Grid.Row="4" Grid.Column="0"/>
+        <TextBox x:Name="PhoneNumberTextBox" Style="{StaticResource BookInfoTextBoxStyle}" Grid.Row="4" Grid.Column="1" Grid.ColumnSpan="2"/>
+
+        <!-- Address -->
+        <TextBlock Text="Address:" Style="{StaticResource BookInfoLabelStyle}" Grid.Row="5" Grid.Column="0"/>
+        <TextBox x:Name="AddressTextBox" Style="{StaticResource BookInfoTextBoxStyle}" Grid.Row="5" Grid.Column="1" Grid.ColumnSpan="2"/>
+
+        <!-- Username -->
+        <TextBlock Text="Username:" Style="{StaticResource BookInfoLabelStyle}" Grid.Row="6" Grid.Column="0"/>
+        <TextBox x:Name="UsernameTextBox" Style="{StaticResource BookInfoTextBoxStyle}" Grid.Row="6" Grid.Column="1" Grid.ColumnSpan="2"/>
+
+        <!-- Password -->
+        <TextBlock Text="Password:" Style="{StaticResource BookInfoLabelStyle}" Grid.Row="7" Grid.Column="0"/>
+        <TextBox x:Name="PasswordTextBox" Style="{StaticResource BookInfoTextBoxStyle}" Grid.Row="7" Grid.Column="1" Grid.ColumnSpan="2"/>
+
+        <!-- Action Buttons -->
+        <Button Content="Update" Style="{StaticResource ActionButtonStyle}" Grid.Row="9" Grid.Column="1"/>
+        <Button Content="Delete" Style="{StaticResource ActionButtonStyle}" Grid.Row="9" Grid.Column="2"/>
+    </Grid> */
